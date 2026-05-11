ba2331df8e470200a31de1d6f7fc1c581c653cab
christian-nieves <thechristiannieves@gmail.com>
2026-05-11 13:57:14 -0400

    Names done

---
+++ src/main/java/com/pluralsight/Hovercraft.java
@@ -0,0 +1,5 @@
+package com.pluralsight;
+
+public class Hovercraft extends Vehicle {
+    private int airCushionPressure;
+}
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private String model;
+    private String color;
+    private int topSpeed;
+    private int fuelCapacity;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+
+
+}
+
+
+
+
