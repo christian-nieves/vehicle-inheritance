add1df02c5e990078f57eb8589ca8369e7b95781
christian-nieves <thechristiannieves@gmail.com>
2026-05-11 14:10:58 -0400

    Vehicle, Moped, Car classes done

--- src/main/java/com/pluralsight/Car.java
+++ src/main/java/com/pluralsight/Car.java
@@ -2,4 +2,12 @@ package com.pluralsight;
 
 public class Car extends Vehicle {
     private int numberOfDoor;
+
+    public void openTrunk() {
+        System.out.println("Trunk Opened.");
+    }
+
+    public void closeTruck() {
+        System.out.println("Trunk Closed.");
+    }
 }
--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -8,7 +8,69 @@ public class Vehicle {
     private int numberOfPassengers;
     private int cargoCapacity;
 
+    public String getModel() {
+        return model;
+    }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void setTopSpeed(int topSpeed) {
+        this.topSpeed = topSpeed;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public void startEngine() {
+        System.out.println("Engine has started!");
+    }
+
+    public void stopEngine() {
+        System.out.println("Engine has stopped.");
+    }
+
+    public void accelerate() {
+        System.out.println("Accelerate");
+    }
+
+    public void brake() {
+        System.out.println("Brake");
+    }
 }
 
 
