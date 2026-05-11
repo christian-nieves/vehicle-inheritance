73406b56b64df6e1f2cca15a298a3d336b69c180
christian-nieves <thechristiannieves@gmail.com>
2026-05-11 14:12:37 -0400

    Semi truck and hovercraft classes done

--- src/main/java/com/pluralsight/Hovercraft.java
+++ src/main/java/com/pluralsight/Hovercraft.java
@@ -2,4 +2,8 @@ package com.pluralsight;
 
 public class Hovercraft extends Vehicle {
     private int airCushionPressure;
+
+    public void hover() {
+        System.out.println("You are hovering!");
+    }
 }
